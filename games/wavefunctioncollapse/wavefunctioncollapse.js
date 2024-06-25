17ee71c78f41e06e17cd775a81241df6a69f7f11
cardy64 <38082763+cardy64@users.noreply.github.com>
2024-06-25 03:23:59 +0200

    wfc is working!!!

--- games/wavefunctioncollapse/wavefunctioncollapse.js
+++ games/wavefunctioncollapse/wavefunctioncollapse.js
@@ -1,7 +1,5 @@
 import MDog from "/MDogEngine/MDogModules/MDogMain.js"
 
-let time = 0;
-
 class TileOption {
     constructor(url, up, right, down, left) {
         this.url = "wfc/" + url + ".png";
@@ -10,16 +8,32 @@ class TileOption {
         this.down = down;
         this.left = left;
     }
+
+    getDir(num) {
+        while (num < 4) num += 4;
+        switch (num % 4) {
+            case 0: return this.up;
+            case 1: return this.right;
+            case 2: return this.down;
+            case 3: return this.left;
+            default: throw new Error("getDir(num) received '" + num + "' not a number 0, 1, 2, or 3.");
+        }
+    }
 }
 
 const g = "grass";
 const p = "path";
 
+function newMaterialList() {
+    return [g, p];
+}
+
 const tileOptions = [
     new TileOption("none", g, g, g, g),
+
     new TileOption("left-right", g, p, g, p),
     new TileOption("up-down", p, g, p, g),
-    new TileOption("down-left", g, g, p, p),
+
     new TileOption("left-up", p, g, g, p),
     new TileOption("right-down", g, p, p, g),
     new TileOption("up-right", p, p, g, g),
@@ -38,40 +52,132 @@ function newOptionList() {
     return ret;
 }
 
-class Tile {
-    constructor() {
-        this.tile = null;
-        this.options = newOptionList();
-        this.lastUpdate = -1;
-    }
-}
-
-let size = 4;
+let size = 10;
 let map = [];
 
 for (let i = 0; i < size; i++) {
     map.push([]);
     for (let j = 0; j < size; j++) {
-        map[i].push(new Tile());
+        map[i].push(newOptionList());
     }
 }
 
 function randFromList(list) {
     const length = list.length;
     if (length === 0) return null;
+    // if (list.includes(0) && Math.random() > 0.05) return 0;
     return list[Math.floor(Math.random()*length)];
 }
 
-function collapse(x, y) {
-    const tile = map[x][y];
-    tile.tile = randFromList(tile.options);
+function getTileOption(optionNum) {
+    return tileOptions[optionNum];
 }
 
+// This function takes a direction and a list of TileOption indexes and
+// returns a set of the materials that go in that direction.
+// 0, 1, 2, 3 - up, right, down, left.
+function getDirOfTile(dir, options) {
+    const materials = [];
+    for (let i = 0; i < options.length; i++) {
+        const optionNum = options[i];
+        const material = getTileOption(optionNum).getDir(dir);
+        if (!materials.includes(material)) {
+            materials.push(material);
+        }
+    }
+    return materials;
+}
+
+function update(x, y) {
+
+    if (x < 0 || y < 0 || x >= size || y >= size) return;
+
+    const optionNums = map[x][y];
+    if (optionNums.length === 1) return;
+
+    let up;
+    if (y-1 >= 0) {
+        up = getDirOfTile(2, map[x][y-1]);
+    } else {
+        up = newMaterialList();
+    }
+
+    let right;
+    if (x+1 < size) {
+        right = getDirOfTile(3, map[x+1][y]);
+    } else {
+        right = newMaterialList();
+    }
+
+    let down;
+    if (y+1 < size) {
+        down = getDirOfTile(0, map[x][y+1]);
+    } else {
+        down = newMaterialList();
+    }
+
+    let left;
+    if (x-1 >= 0) {
+        left = getDirOfTile(1, map[x-1][y]);
+    } else {
+        left = newMaterialList();
+    }
+
+    let changedOptionNums = false;
+
+    for (let i = 0; i < optionNums.length; i++) {
+        const optionNum = optionNums[i];
+        const tileOption = getTileOption(optionNum);
+        if ((! up.includes(tileOption.up)) ||
+            (! right.includes(tileOption.right)) ||
+            (! down.includes(tileOption.down)) ||
+            (! left.includes(tileOption.left))) {
+            optionNums.splice(i, 1);
+            i -= 1;
+            changedOptionNums = true;
+        }
+    }
+
+    if (changedOptionNums) {
+        update(x-1, y);
+        update(x+1, y);
+        update(x, y-1);
+        update(x, y+1);
+    }
+}
+
+function collapse(x, y) {
+    const optionNums = map[x][y];
+    if (optionNums.length === 1) return;
+    map[x][y] = [randFromList(optionNums)];
+    update(x-1, y);
+    update(x+1, y);
+    update(x, y-1);
+    update(x, y+1);
+}
+
+let done = false;
+
 function main() {
+    if (!done) {
+        done = true;
+        for (let i = 0; i < size; i++) {
+            for (let j = 0; j < size; j++) {
+                collapse(i, j);
+            }
+        }
+    }
+
     for (let i = 0; i < size; i++) {
         for (let j = 0; j < size; j++) {
-            collapse(i, j);
-            time += 1;
+            const optionNums = map[i][j];
+            if (optionNums.length === 1) {
+                const tileOption = getTileOption(optionNums[0]);
+                MDog.Draw.image(tileOption.url, i*16, j*16);
+                if ((i+j)%2 === 0) {
+                    MDog.Draw.rectangleFill(i*16, j*16, 16, 16, "#f0000022");
+                }
+            }
         }
     }
 }
