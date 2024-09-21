e803b13a5c95105ef7b55db70be83136ccf614b9
Milo Kesteloot <milo@teamten.com>
2024-09-20 17:52:38 -0700

    Scroll Test Fix

--- MDogModules/MDogMain.js
+++ MDogModules/MDogMain.js
@@ -30,6 +30,8 @@ class MDog {
         window.addEventListener("keydown", function(e) {
             if(["Space","ArrowUp","ArrowDown","ArrowLeft","ArrowRight"].indexOf(e.code) > -1) {
                 e.preventDefault();
+                e.stopPropagation();
+                e.stopImmediatePropagation();
             }
         }, false);
 
