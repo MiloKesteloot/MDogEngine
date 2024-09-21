95b606d6d3d05d338359cb3818bad4721f87843c
Milo Kesteloot <milo@teamten.com>
2024-09-20 18:24:13 -0700

    Testing Scroll, removed e.stopPropagation();

--- MDogModules/MDogMain.js
+++ MDogModules/MDogMain.js
@@ -30,7 +30,7 @@ class MDog {
         window.addEventListener("keydown", function(e) {
             if(["Space","ArrowUp","ArrowDown","ArrowLeft","ArrowRight"].indexOf(e.code) > -1) {
                 e.preventDefault();
-                e.stopPropagation();
+                // e.stopPropagation();
             }
         }, false);
 
